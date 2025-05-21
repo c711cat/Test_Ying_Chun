2e5d94dd62f4a69696cbd716d40589b70ef86e2b
c711cat <c711cat@gmail.com>
2025-05-21 15:39:58 +0800

    create initial screen

---
+++ src/components/Screen.vue
@@ -0,0 +1,104 @@
+<template>
+  <main class="wrap">
+     <section class="btnBox">
+      <div><button type="button" class="blueBtn">Add</button></div>
+      <div><button type="button" class="greenBtn">Save</button></div>
+      <div><button type="button" class="redBtn">Update</button></div>
+    </section>
+    <table class="">
+      <thead>
+        <tr>
+          <th class="head">Name</th>
+          <th class="head">Birthday</th>
+          <th class="head">Salary</th>
+          <th class="head">Address</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item,index) in person" :key="item.name+index">
+          <td><input type="text" name="name" id="" :value="item.name"></td>
+          <td><input type="date" name="birthdat" id="" :value="item.birthday"></td>
+          <td><input type="range" name="salary" min="0" max="100000" id="" :value="item.salary"></td>
+          <td><input type="text" name="address" id="" :value="item.address"></td>
+        </tr>
+      </tbody>
+    </table>
+  </main>
+</template>
+
+<script>
+export default{
+  data(){
+    return{
+      person:[
+      {name:'anny',birthday:'1988-10-10',salary:100000,address:'djvjks'},
+      {name:'anny',birthday:'1988-10-10',salary:10000,address:'djvjks'},
+      {name:'anny',birthday:'1988-10-10',salary:10000,address:'djvjks'},
+      {name:'anny',birthday:'1988-10-10',salary:10000,address:'djvjks'},
+      {name:'anny',birthday:'1988-10-10',salary:10000,address:'djvjks'}],
+    }
+  }
+}</script>
+
+<style lang="scss" scoped>
+button {
+  border-radius: 10%;
+  padding: 11px;
+  border: none;
+  color: white;
+  cursor: pointer;
+  font-size: 16px;
+}
+
+button:hover {
+filter:brightness(1.2);
+}
+
+table {
+  width: 100%;
+  border-collapse: collapse;
+}
+
+td , th{
+  padding: 6px 10px;
+  border-bottom:1px solid #e4e3e6;
+}
+
+input{
+  width: 70%;
+  padding: 7px 4px;
+  border-radius: 3px;
+  border: 1px solid#777777;
+  font-size: 16px;
+}
+
+.wrap {
+  margin: 50px auto;
+  width: 80%;
+}
+
+.btnBox {
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  margin-bottom: 25px;
+}
+
+.blueBtn {
+  background-color: #0868f9;
+}
+
+.greenBtn {
+  background-color: #1b7b4b;
+}
+
+.redBtn {
+  background-color: #d72d3d;
+}
+
+.head{
+  text-align: left;
+  padding-bottom: 12px;
+}
+
+</style>
